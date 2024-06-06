5a136902dd0d5234bd2db122cf26e6d5005fba73
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-06-06 18:21:05 +0100

    feat: Implemented AmphibiansTopAppBar()

---
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -0,0 +1,31 @@
+package com.example.amphibians.ui
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.amphibians.R
+
+@Composable
+fun AmphibiansApp() {
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AmphibiansTopAppBar(
+    modifier: Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge
+            )
+    },
+        modifier = modifier
+    )
+}
