d767c2792b352dbc2e9d3841cbad47806411c428
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-06-19 13:21:31 +0100

    feat: Implemented loading_animation.xml file

--- app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.amphibians.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -9,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,14 +21,14 @@ import com.example.amphibians.ui.theme.AmphibiansTheme
 
 @Composable
 fun HomeScreen(
-    ambhibiansUiState: String,
+    ambhibiansUiState: AmphibiansUiState,
     modifier: Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp),
     ) {
     when (ambhibiansUiState) {
         is AmphibiansUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
         is AmphibiansUiState.Success -> ResultScreen(
-            photos = ambhibiansUiState,
+            photos = ambhibiansUiState.photos,
             modifier = modifier.padding(top = contentPadding.calculateTopPadding())
         )
         is AmphibiansUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
@@ -35,6 +39,29 @@ fun HomeScreen(
  * LoadingScreen displaying a loading spinner.
  */
 @Composable
+fun LoadingScreen(modifier: Modifier) {
+    Image(
+        modifier = modifier,
+        painter = painterResource(R.drawable.loading_img),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+/**
+ * ErrorScreen displaying an error message.
+ */
+@Composable
+fun ErrorScreen(modifier: Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_connection_error), contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+    }
+}
 
 /**
  * ResultScreen displaying number of photos retrieved.
